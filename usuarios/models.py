e64efe70018c16f269471c792981fc0c530ab72a
rolansor <rolansor@hotmail.com>
2015-06-03 02:07:42 -0500

    Modificacion del Modelo con el nuevo estandar y la posibilidad de trabajar con la autenticacion de usuarios que provee DJango.

--- usuarios/models.py
+++ usuarios/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 
 class Ubicacion(models.Model):
-    idubicacion = models.AutoField(db_column='idUbicacion', primary_key=True)  
+    idubicacion = models.AutoField(primary_key=True)
     pais = models.CharField(max_length=45)
     ciudad = models.CharField(max_length=45)
     abreviatura = models.CharField(max_length=45)
@@ -15,24 +15,24 @@ class Ubicacion(models.Model):
 
 
 class Perfil(User):
-    idperfil = models.AutoField(db_column='idPerfil', primary_key=True)
+    idperfil = models.AutoField(primary_key=True)
     cedula = models.CharField(unique=True, max_length=10)
     foto = models.TextField()
     web = models.CharField(max_length=100)
     telefono = models.CharField(max_length=16)
-    fecharegistro = models.DateTimeField(db_column='fechaRegistro') 
-    ipregistro = models.CharField(db_column='ipRegistro', max_length=15)  
+    fecharegistro = models.DateTimeField()
+    ipregistro = models.CharField(max_length=15)
     reputacion = models.DecimalField(max_digits=4, decimal_places=0)
     estado = models.IntegerField()
     privacidad = models.CharField(max_length=11)
-    fkubicacion = models.ForeignKey(Ubicacion, db_column='fkUbicacion') 
+    fkubicacion = models.ForeignKey(Ubicacion)
 
     class Meta:
         db_table = 'Perfil'
 
 
 class Institucion(models.Model):
-    idinstitucion = models.AutoField(db_column='idInstitucion', primary_key=True)
+    idinstitucion = models.AutoField(primary_key=True)
     nombre = models.CharField(unique=True, max_length=45)
     siglas = models.CharField(max_length=12)
     logo = models.TextField()
@@ -40,45 +40,45 @@ class Institucion(models.Model):
     mision = models.CharField(max_length=500)
     ubicacion = models.CharField(max_length=45)
     web = models.CharField(max_length=45)
-    recursosofrecidos = models.CharField(db_column='recursosOfrecidos', max_length=200, blank=True, null=True)
+    recursosofrecidos = models.CharField(max_length=200, blank=True, null=True)
 
     class Meta:
         db_table = 'Institucion'
 
 
 class Membresia(models.Model):
-    idmembresia = models.AutoField(db_column='idMembresia', primary_key=True)
-    esadministrator = models.IntegerField(db_column='esAdministrator') 
+    idmembresia = models.AutoField(primary_key=True)
+    esadministrator = models.IntegerField()
     cargo = models.CharField(max_length=45)
     descripcion = models.CharField(max_length=45)
     fecha = models.CharField(max_length=45)
-    ippeticion = models.CharField(db_column='ipPeticion', max_length=45) 
+    ippeticion = models.CharField (max_length=45)
     estado = models.IntegerField(blank=True, null=True)
-    fkinstitucion = models.ForeignKey(Institucion, db_column='fkInstitucion')
-    fkusuario = models.ForeignKey('User', db_column='fkUsuario') 
+    fkinstitucion = models.ForeignKey(Institucion)
+    fkusuario = models.ForeignKey(User)
 
     class Meta:
         db_table = 'Membresia'
 
 
 class Mensaje(models.Model):
-    idmensaje = models.AutoField(db_column='idMensaje', primary_key=True) 
+    idmensaje = models.AutoField(primary_key=True)
     mensaje = models.CharField(max_length=1000)
     fecha = models.DateTimeField()
     asunto = models.CharField(max_length=45)
-    fkemisor = models.ForeignKey('User', db_column='fkEmisor',related_name='fkemisor') 
-    fkreceptor = models.ForeignKey('User', db_column='fkReceptor',related_name='fkreceptor')  
+    fkemisor = models.ForeignKey(User,related_name='fkemisor')
+    fkreceptor = models.ForeignKey(User,related_name='fkreceptor')
 
     class Meta:
         db_table = 'Mensaje'
 
 
 class Peticion(models.Model):
-    idpeticion = models.AutoField(db_column='idPeticion', primary_key=True) 
+    idpeticion = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=45)
     codigo = models.CharField(max_length=128)
     usado = models.IntegerField()
-    fkusuario = models.ForeignKey('User', db_column='fkUsuario')  
+    fkusuario = models.ForeignKey(User)
 
     class Meta:
         db_table = 'Peticion'
