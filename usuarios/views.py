8a89e3410dbbd9aa99dc57736c50fa84c8b0ee2a
Angel <adguale@espol.edu.ec>
2015-06-06 17:00:25 -0500

    Agregar csrf token al form

--- usuarios/views.py
+++ usuarios/views.py
@@ -8,8 +8,32 @@ from django.contrib.auth.decorators import login_required
 
 def registro_institucion(request):
 
-    return render_to_response('Institucion_Sign-up.html',{})
+	return render_to_response('Institucion_Sign-up.html',{})
 
+"""
+Autor: Angel Guale
+Nombre de funcion: registro_usuario 
+Entrada: request GET
+Salida: Formulario de registro usuario
+Responde con un formulario vacio de registro de usuario
+"""
 def registro_usuario(request):
-    return render_to_response('Usuario_Sign-up.html',{})
+	if request.method=='POST':
+		print request.POST
+		username=request.POST['username']
+		nombres=request.POST['nombres']
+		apellidos=request.POST['apellidos']
+		cedula=request.POST['cedula']
+		cargo=request.POST['cargo']
+		actividad=request.POST['actividad']
+		website=request.POST['website']
+		email=request.POST['email']
+		ciudad=request.POST['ciudad']
+		fechaNacimiento=request.POST['fechaNacimiento']
+		areasInteres=request.POST['areasInteres']
+
+
+		pass	
+	else:
+		return render_to_response('Usuario_Sign-up.html',{})
 
