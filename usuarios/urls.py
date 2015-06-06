c404772c4c1701a7280f433541714e042c6c0bd7
omnilasher <raywarcraft@hotmail.com>
2015-06-06 14:23:49 -0500

    Linkeada la url para el registro de institucion

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,1 +1,12 @@
 __author__ = 'Ray'
+
+from views import *
+from django.conf.urls import patterns, url
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = patterns('',
+        url(r'^registro_institucion[/]?$',registro_institucion,name='index'),
+
+)
