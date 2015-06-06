cfb2ed6d44f5304ba83bce8e5db331c3b3853dfd
omnilasher <raywarcraft@hotmail.com>
2015-06-06 14:23:13 -0500

    añadida la vista para el registro de institucion...validacion coming soon

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# -*- encoding: utf-8 -*-
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, render_to_response
+from django.template.context_processors import csrf
+from models import *
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def registro_institucion(request):
+
+    return render_to_response('Institucion_Sign-up.html',{})
