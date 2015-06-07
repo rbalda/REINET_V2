b3d5f4cc55e27ed91be579ad7d4dcee03a67b2c7
Your Name <patitodelt22@gmail.com>
2015-06-06 19:31:11 -0500

    Funcion Sign-in usuario y mostrar datos de usuario desde la base(parcialmente)

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -7,6 +7,11 @@ from django.conf.urls.static import static
 
 
 urlpatterns = patterns('',
+
+		url(r'^signIn[/]?$','usuarios.views.signIn', name='signIn'),
+		url(r'^autentificacion[/]?$','usuarios.views.autentificacion',name='autentificacion'),
+		url(r'^signUp[/]?$','usuarios.views.signUp', name='signUp'),
+		url(r'^perfilUsuario[/]?$','usuarios.views.perfilUsuario', name='perfilUsuario'),
         url(r'^registro_institucion[/]?$',registro_institucion,name='index'),
         url(r'^registro_usuario[/]?$',registro_usuario,name='index'),
     	url(r'^index[/]?$',index,name='index'),
