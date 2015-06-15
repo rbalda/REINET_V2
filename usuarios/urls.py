e5fb10f657da265809c4b755a187270380296b59
Angel <adguale@espol.edu.ec>
2015-06-14 22:20:41 -0500

    corrigiendo de nuevo lo que alguien más hizo porque no le corrio

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -23,7 +23,6 @@ urlpatterns = patterns('',
         url(r'^csrf_failure[/]?$', 'usuarios.views.csrf_failure', name='csrf_failure'),
         url(r'^editar_perfil_institucion[/]?$', 'usuarios.views.modificarPerfilInstitucion', name='editarPerfilInstitucion'),
 
-
         url(r'^usuario[/]?$','usuarios.views.verCualquierUsuario', name='verUsuario'),
         
         url(r'^verificar_username[/]?$',verificar_username, name="verificar_username"),
