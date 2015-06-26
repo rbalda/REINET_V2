e8c03190378053cd72efe4faa8dc9ce9898be614
rolansor <rolansor@hotmail.com>
2015-06-26 02:52:46 -0500

    Se añadio el template de administrar membresias, su url, y su controlador, se cambio tal que al entrar a aministrar institucion el nombre del perfil cambie a las siglas de la institucion, se corrigio el modelo con el que se llamaba al perfil de la institucion y se valido para que no se pueda administrar si no se es administrador de algo.

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -8,47 +8,53 @@ from django.conf.urls.static import static
 
 urlpatterns = patterns('',
 
-		url(r'^cerrarSesion[/]?$','usuarios.views.cerrarSesion', name='cerrarSesion'),
+        url(r'^index[/]?$',index,name='index'),
+        url(r'^terminosYcondiciones[/]?$','usuarios.views.terminosCondiciones', name='terminosYcondiciones'),
+
 		url(r'^iniciarSesion[/]?$','usuarios.views.iniciarSesion', name='iniciarSesion'),
-        url(r'^recuperarPassword[/]?$','usuarios.views.recuperarPassword',name='recuperarPassword'),
-        url(r'^suspender_usuario[/]?$',suspenderUsuario,name='suspenderUsuario'),
-		url(r'^perfilUsuario[/]?$','usuarios.views.perfilUsuario', name='perfilUsuario'),
-
-        url(r'^perfilInstitucion[/]?$', 'usuarios.views.perfilInstitucion', name='perfilInstitucion'),
-        url(r'^envioSolicitud[/]?$','usuarios.views.registrarSolicitud', name="envio_solicitud"),
-        url(r'^registro_institucion/(?P<codigo>\w{0,50})[/]?$',registro_institucion,name='index'),
-        url(r'^verPeticiones[/]?$',verPeticiones,name='verPeticiones'),
-        url(r'^aceptarPeticiones[/]?$',aceptarPeticiones,name='aceptarPeticiones'),
-
-        url(r'^registro_usuario[/]?$',registro_usuario,name='index'),
-    	url(r'^index[/]?$',index,name='index'),
-    	url(r'^[/]?$',index,name='index'),
-    	url(r'^verificar_codigo[/]?$',verificarCodigo,name='institucion'),
-    	url(r'^terminosYcondiciones[/]?$','usuarios.views.terminosCondiciones', name='terminosYcondiciones'),
-    	url(r'^inicioUsuario[/]?$','usuarios.views.inicio',name='inicioUsuario'),
-        url(r'^editar_usuario[/]?$', 'usuarios.views.editar_usuario' ,name='index'),
+		url(r'^cerrarSesion[/]?$','usuarios.views.cerrarSesion', name='cerrarSesion'),
         url(r'^enviarEmailPassword[/]?$','usuarios.views.enviarEmailPassword',name='enviarEmailPassword'),
         url(r'^csrf_failure[/]?$', 'usuarios.views.csrf_failure', name='csrf_failure'),
-        url(r'^editar_perfil_institucion[/]?$', 'usuarios.views.modificarPerfilInstitucion', name='editarPerfilInstitucion'),
+		url(r'^recuperarPassword[/]?$','usuarios.views.recuperarPassword',name='recuperarPassword'),
 
+
+        url(r'^registro_usuario[/]?$',registro_usuario,name='index'),
+        url(r'^registro_institucion/(?P<codigo>\w{0,50})[/]?$',registro_institucion,name='index'),
+
+        url(r'^inicioUsuario[/]?$','usuarios.views.inicio',name='inicioUsuario'),
+        url(r'^perfilUsuario[/]?$','usuarios.views.perfilUsuario', name='perfilUsuario'),
+        url(r'^editar_usuario[/]?$', 'usuarios.views.editar_usuario' ,name='index'),
+        url(r'^suspender_usuario[/]?$',suspenderUsuario,name='suspenderUsuario'),
         url(r'^usuario/(?P<username>\w{0,250})[/]?$','usuarios.views.verCualquierUsuario', name='verUsuario'),
-        
-        url(r'^verificar_username[/]?$',verificar_username, name="verificar_username"),
-        url(r'^verificar_cedula[/]?$',verificar_cedula, name="verificar_cedula"),
-        url(r'^verificar_email[/]?$',verificar_email, name="verificar_email"),
-       
-        url(r'^GenerarCodigo[/]?$', generar_codigo, name='GenerarCodigo'),
-        url(r'^getCiudades[/]?$',obtenerCiudades,name='institucion'),
-        url(r'^institucion/(?P<institucionId>\w{0,50})[/]?$', verPerfilInstituciones,name='institucion'),
 
-
-        url(r'^api/buscar_institucion[/]?',InstitucionBusqueda.as_view(),name='buscar_institucion'),
-        url(r'^api/buscar_usuario[/]?',PerfilBusqueda.as_view(),name='buscar_usuario'),
 
         url(r'^BandejaDeEntrada[/]?$', ver_bandeja_entrada, name='BandejaDeEntrada'),
         url(r'^enviarMensaje[/]?$', enviarMensaje, name='enviarMensaje'),
         url(r'^verMensaje[/]?$', verMensaje, name='verMensaje'),
         url(r'^mensajesEnviados[/]?$', mensajesEnviados, name='mensajesEnviados'),
+
+
+        url(r'^perfilInstitucion[/]?$', 'usuarios.views.perfilInstitucion', name='perfilInstitucion'),
+        url(r'^editar_perfil_institucion[/]?$', 'usuarios.views.modificarPerfilInstitucion', name='editarPerfilInstitucion'),
+        url(r'^envioSolicitud[/]?$','usuarios.views.registrarSolicitud', name="envio_solicitud"),
+        url(r'^verPeticiones[/]?$',verPeticiones,name='verPeticiones'),
+        url(r'^aceptarPeticiones[/]?$',aceptarPeticiones,name='aceptarPeticiones'),
+        url(r'^verificar_codigo[/]?$',verificarCodigo,name='institucion'),
+        url(r'^institucion/(?P<institucionId>\w{0,50})[/]?$', verPerfilInstituciones,name='institucion'),
+        url(r'^AdministrarMembresias[/]?$', 'usuarios.views.administrar_membresias', name='AdministrarMembresias'),
+
+
+        url(r'^verificar_username[/]?$',verificar_username, name="verificar_username"),
+        url(r'^verificar_cedula[/]?$',verificar_cedula, name="verificar_cedula"),
+        url(r'^verificar_email[/]?$',verificar_email, name="verificar_email"),
+        url(r'^GenerarCodigo[/]?$', generar_codigo, name='GenerarCodigo'),
+        url(r'^getCiudades[/]?$',obtenerCiudades,name='institucion'),
+
+
+        url(r'^api/buscar_institucion[/]?',InstitucionBusqueda.as_view(),name='buscar_institucion'),
+        url(r'^api/buscar_usuario[/]?',PerfilBusqueda.as_view(),name='buscar_usuario'),
+
+
 )
 # url(regex=r'^check_cedula/(?P<cedula>\d+)/$',
 # view=CedulaCheck.as_view(),
--- usuarios/views.py
+++ usuarios/views.py
@@ -650,29 +650,33 @@ Autores: Ray Montiel, Edinson Sánchez y Roberto Yoncon
 Nombre de funcion: perfilInstitucion
 Entrada: request GET
 Salida: Perfil de Institucion
+Descripcion:
+Ultima Modificacion: Rolando Sornoza - Se corrigio la logica de la funcion.
 """
 
 @login_required
-def perfilInstitucion(request): #Error 10, nombre inadecuado de la funcion
-	session = request.session['id_usuario'] #Error 10, usar palabras en español
+def perfilInstitucion(request):
+	session = request.session['id_usuario']
 	usuario = Perfil.objects.get(id=session)
 	args = {}
 
 	if usuario is not None:
+		#Guardo en la variable de sesion a usuario.
 		args['usuario'] = usuario
-		membresia = Membresia.objects.filter(fk_usuario=usuario.id).first()
-		print "sadhas"
-		if membresia.es_administrator:
-			print "entre"
-			print membresia.id_membresia
-			institucion = Institucion.objects.get(id_institucion=membresia.fk_institucion.id_institucion)
-			#print institucion
-			numMiembros = Membresia.objects.filter(fk_institucion_id=institucion.id_institucion).values_list('fk_usuario_id', flat=True).distinct().count()
-			args['institucion'] = institucion
-			args['numMiembros'] = numMiembros
+		#Saco la membresia de la que soy administrador, y me aseguro que no sea la institucion independiente.
+		membresia = Membresia.objects.filter(es_administrator=1,fk_usuario=usuario.id).exclude(fk_institucion=1).first()
+		#Si existe un registro es por que soy administrador de una institucion.
+		if membresia is not None :
+			if membresia.es_administrator :
+				print membresia.id_membresia
+				institucion = Institucion.objects.get(id_institucion=membresia.fk_institucion.id_institucion)
+				numMiembros = Membresia.objects.filter(fk_institucion_id=institucion.id_institucion).values_list('fk_usuario_id', flat=True).distinct().count()
+				args['institucion'] = institucion
+				args['numMiembros'] = numMiembros
+		#Sino, simplemente no tengo ninguna institucion a mi cargo y regreso a mi perfil
 		else:
-			print "aca"
 			args['error1'] = "Usted no es miembro de ninguna Institucion"
+			return HttpResponseRedirect('/perfilUsuario/')
 
 	else:
 		args['error'] = "Error al cargar los datos"
@@ -1265,3 +1269,16 @@ def mensajesEnviados(request):
 	args['range']=range(len(mensajes))
 	args['es_admin'] = request.session['es_admin']
 	return render_to_response('mensajes_enviados.html',args)
+
+
+"""
+Autor: Rolando Sornoza, Roberto Yoncon
+Nombre de funcion: administrar_membresias
+Entrada: request POST
+Salida: Listado de las memebresias correspondientes la Institucion.
+Descripción: Muestra las membresias pendientes y aceptadas y permite modificarlas.
+"""
+
+def administrar_membresias(request):
+	args = {}
+	return render_to_response('administrar_membresias.html', args)
