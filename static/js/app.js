d5b574f039532f8df8b09e77d2ca9c29dd58bbaa
René Balda <rabt89@gmail.com>
2015-06-28 21:16:22 -0500

    Agregando cliente Swampdragon Autor: René Balda

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,26 @@
-var redInn = angular.module('redInn',['ngResource','ngAnimate','ngRoute','ngCookies']);
+var redInn = angular.module('redInn',['ngResource','ngAnimate','ngRoute','ngCookies','SwampDragonServices']);
 
+//Configuracion de angular para que no se confunda con sintaxis django
 redInn.config(['$interpolateProvider','$resourceProvider',function( $interpolateProvider,$resourceProvider){
     $interpolateProvider.startSymbol('[[');
     $interpolateProvider.endSymbol(']]');
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
 
+//Agregando csrf token en cada peticion ajax que realice angular
 redInn.config(['$httpProvider', function($httpProvider) {
     $httpProvider.defaults.xsrfCookieName = 'csrftoken';
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
 }]);
 
+//definiendo urls contantes
 redInn.constant('urls', {
        BASE: '/',
        BASE_API: '/api'
 });
 
+
+//Creando servicio para busqueda de entidades en la barra de busquedas
 redInn.factory('Entidades',['$http','urls',function($http,urls){
     return{
         get_usuarios: function (data, success, error) {
@@ -31,6 +36,7 @@ redInn.factory('Entidades',['$http','urls',function($http,urls){
     }
 }]);
 
+//controlador que hara la busqueda de los usuario y los mostrara en pantalla
 redInn.controller('ControladorBusqueda',['$scope','Entidades',function($scope,Entidades){
     $scope.busqueda_entrada = null;
     $scope.lista_usuarios = [];
@@ -78,4 +84,43 @@ redInn.controller('ControladorBusqueda',['$scope','Entidades',function($scope,En
             $scope.lista_instituciones = [];
         }
     }
+}]);
+
+
+redInn.controller('MensajesControllers',['$scope','$dragon',function($scope,$dragon){
+    $scope.mensaje = {};
+    $scope.mensajes = [];
+    $scope.channel = 'mensaje';
+    $scope.nroMensajes=0;
+
+
+
+    $dragon.onReady(
+        function(){
+            $dragon.subscribe('mensaje-router',$scope.channel,{}).then(function(response){
+                    $scope.dataMapper = new DataMapper(response.data);
+                });
+            $dragon.getSingle('mensaje-router',{id:1}).then(function(response){
+                $scope.mensaje = response.data;
+            });
+            $dragon.getList('mensaje-router',{leido:false}).then(function (response) {
+                $scope.mensajes = response.data;
+            });
+
+        });
+
+    $dragon.onChannelMessage(function(channels, message) {
+        if (indexOf.call(channels, $scope.channel) > -1) {
+            $scope.$apply(function() {
+                $scope.dataMapper.mapData($scope.mensajes, message);
+            });
+        }
+    });
+//    $dragon.onChannelMessage(function(channels, message) {
+//    if (indexOf.call(channels, $scope.channel) > -1) {
+//        this.dataMapper.mapData($scope.todoItems, message);
+//        $scope.$apply();
+//    }
+//});
+
 }]);
