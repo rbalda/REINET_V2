3652acda9afd43cf55f8c67b26dc31f4902acd4e
Pedro Aim <pedro_aim@hotmail.com>
2015-06-30 01:30:39 -0500

    modificacion perfil otra institucion

--- REINET/settings.py
+++ REINET/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 )
 
 REST_FRAMEWORK = {
