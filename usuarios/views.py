7bc99ac3cc986e541fca72f8a9b0d91114a67fe8
Josanvel <josanvel@espol.edu.ec>
2015-07-04 15:05:27 -0500

    Plantilla de Editar contraseña

--- usuarios/views.py
+++ usuarios/views.py
@@ -520,6 +520,90 @@ def cerrarSesion(request):
 
 
 """
+Autor: Jose Velez
+Nombre de función: editarContrasena
+Parámetros:request
+Salida: Redirecciona al perfil de usuario
+Descripción: Esta funcion edita la contraseña del usuario y la actualiza en la bases de datos
+"""
+
+def editarContrasena(request):
+	idFoto = 1
+	session = request.session['id_usuario']
+	usuario = Perfil.objects.get(id=session)
+	args = {}
+
+	if usuario is not None:
+		args['usuario'] = usuario
+	else:
+		args['error'] = "Error al cargar los datos"
+
+	if request.method == 'POST':
+		#print request.POST
+		nombres = request.POST['nombres']
+		apellidos = request.POST['apellidos']
+		#cedula=request.POST['cedula']
+		#cargo=request.POST['cargo']
+		telefono = request.POST['telefono']
+		#actividad=request.POST['actividad']
+		website = request.POST['website']
+		email = request.POST['email']
+		try:
+			foto = request.FILES['imagen']
+		except:
+			idFoto = 0 #ID para no guardar foto de noPicture
+			foto = "../../media/noPicture.png"
+		 #Explicar como funciona el array de privacidad.
+		try:
+			#privacidadNom=request.POST['PrivacidadNombre']
+			#privacidadApe=request.POST['PrivacidadApellido']
+			privacidadCed = request.POST['PrivacidadCedula']
+			privacidadTel = request.POST['PrivacidadTelefono']
+			privacidadWeb = request.POST['PrivacidadWeb']
+			privacidadMai = request.POST['PrivacidadMail']
+			#if privacidadWeb=="1" and privacidadMai=="1":
+			#	privacidadWeb='3'
+			#elif privacidadWeb=="0" and privacidadMai=="1":
+			#	privacidadWeb='2'
+			#privacidad=privacidadNom+privacidadApe+privacidadCed+privacidadTel+privacidadWeb
+			privacidad = privacidadCed + privacidadTel + privacidadWeb + privacidadMai
+
+		except:
+			privacidad = 1111
+
+		print foto
+		perfil = usuario
+		perfil.first_name = nombres
+		perfil.last_name = apellidos
+		#perfil.cedula=cedula
+		#perfil.cargo=cargo
+		#perfil.actividad=actividad
+		perfil.web = website
+		perfil.email = email
+		#perfil.ciudad=ciudad
+		#perfil.fechaNacimiento=fechaNacimiento
+		#perfil.areasInteres=areasInteres
+		perfil.fecharegistro = datetime.datetime.now()
+		perfil.telefono = telefono
+		#ubicacion=Ubicacion.objects.get(idubicacion=1)
+		#perfil.fkubicacion=ubicacion
+		perfil.privacidad = privacidad
+		if idFoto != 0:
+			perfil.foto = foto
+		perfil.save()
+
+
+
+		return HttpResponseRedirect('/perfilUsuario/')
+	else:
+		user = request.user
+		args['es_admin']=request.session['es_admin']
+		args.update(csrf(request))
+		print args
+		return render_to_response('Usuario_Edit_Password.html', args)
+
+
+"""
 Autor: Jose Velez - Leonel Ramirez
 Nombre de función: editar_usuario
 Parámetros:request
