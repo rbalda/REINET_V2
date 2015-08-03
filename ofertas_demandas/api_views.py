789741ec88d8334fcd16e2f7f3119632583e4322
René Balda <rabt89@gmail.com>
2015-08-03 03:51:42 -0500

    views para ofertas

---
+++ ofertas_demandas/api_views.py
@@ -0,0 +1,14 @@
+
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+from ofertas_demandas.models import Oferta
+from ofertas_demandas.permissions import SiEsPropietarioOEstaEnAlcance
+from ofertas_demandas.serializers import OfertaSerializador
+
+__author__ = 'rbalda'
+
+
+class OfertaViewSet(ModelViewSet):
+    queryset = Oferta.objects.all()
+    serializer_class = OfertaSerializador
+    permission_classes = (IsAuthenticated,SiEsPropietarioOEstaEnAlcance)
