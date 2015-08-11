99cc5b5785b5d2fe86b650ae16552d99a96aa09d
René Balda <rabt89@gmail.com>
2015-08-10 23:30:44 -0500

    la busqueda de las ofertas se realizara con con el codigo

--- ofertas_demandas/api_views.py
+++ ofertas_demandas/api_views.py
@@ -2,6 +2,7 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from ofertas_demandas.models import Oferta
+from ofertas_demandas.pagination import PaginacionPorDefecto
 from ofertas_demandas.permissions import SiEsPropietarioOEstaEnAlcance
 from ofertas_demandas.serializers import OfertaSerializador
 
@@ -12,3 +13,6 @@ class OfertaViewSet(ModelViewSet):
     queryset = Oferta.objects.all()
     serializer_class = OfertaSerializador
     permission_classes = (IsAuthenticated,)
+    lookup_field = 'codigo'
+    pagination_class = PaginacionPorDefecto
+
