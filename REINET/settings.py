86bef0a0d6f66dd0886ad569787d0b74f798cb54
leferami <leferami@espol.edu.ec>
2015-08-23 03:37:28 -0500

    corregir conflictos

--- REINET/settings.py
+++ REINET/settings.py
@@ -46,11 +46,7 @@ INSTALLED_APPS = (
 	'ipware',
 	'usuarios',
 	'ofertas_demandas',
-<<<<<<< HEAD
 	'incubacion',
-=======
-    'incubacion'
->>>>>>> 31cf350c0d42cc4aeb27c5410896184729def6e5
 )
 
 MIDDLEWARE_CLASSES = (
