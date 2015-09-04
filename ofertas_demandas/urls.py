37bb8015f29a62fed9da6155320ea38363958d01
CrobbyStyle <tito_yoncon_ch@hotmail.com>
2015-09-03 19:16:49 -0500

    Corrigiendo la bronzada del dia

--- ofertas_demandas/urls.py
+++ ofertas_demandas/urls.py
@@ -52,13 +52,10 @@ urlpatterns = patterns('',
                        url(r'^aceptarComentarioDemanda/(?P<id_comentario>\w{0,250})[/]?$','ofertas_demandas.views.aceptarComentarioDemanda', name='aceptarComentarioDemanda'),
                        url(r'^rechazarComentarioDemanda/(?P<id_comentario>\w{0,250})[/]?$','ofertas_demandas.views.rechazarComentarioDemanda', name='rechazarComentarioDemanda'),
                        url(r'^administrarBorradorDemanda/(?P<id_demanda>\w{0,250})[/]?$','ofertas_demandas.views.administrar_Borrador_Demanda', name='administrarBorrador'),
-<<<<<<< HEAD
                        url(r'^EditarBorradorDemanda/(?P<id_demanda>\w{0,250})[/]?$','ofertas_demandas.views.editar_borrador_demanda', name='EditarBorradorDemanda'),
                        url(r'^PublicarBorradorDemanda/(?P<id_demanda>\w{0,250})[/]?$','ofertas_demandas.views.publicar_borrador_demanda', name='PublicarBorradorDemanda'),
-=======
                        url(r'^resolverDemanda[/]?$', 'ofertas_demandas.views.resolverDemanda', name='resolverDemanda'),
                        url(r'^ofertaResuelveDemanda[/]?$', 'ofertas_demandas.views.ofertaResuelveDemanda', name='ofertaResuelveDemanda'),
->>>>>>> ce69c567a6914d25053c1b460cd18628978163c6
                        )
 
 urlpatterns += routers.ofertas_routers
