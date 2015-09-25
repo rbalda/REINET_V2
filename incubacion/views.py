c0ff41f93a752baada035e88904d803a67935e05
sjcastro <sjcastro@espol.edu.ec>
2015-09-25 09:04:42 -0500

    Guardar convocatoria revirtiendo cambios. Funciona como debe ser

--- incubacion/views.py
+++ incubacion/views.py
@@ -676,8 +676,7 @@ def admin_ver_incubacion(request, id_incubacion):
     session = request.session['id_usuario']
     usuario = Perfil.objects.get(id=session)
     args = {}
-    args['mensajeError'] = request.session['mensajeError']
-    args['mensajeAlerta'] = request.session['mensajeAlerta']
+    args.update(csrf(request))
     args['es_admin'] = request.session['es_admin']
 
 
@@ -1469,6 +1468,7 @@ class Autocompletar_Consultor(APIView):
         return response
 
 
+
 """
 Autor: Sixto Castro
 Nombre de funcion: GuardarConvocatoria
@@ -1476,21 +1476,23 @@ Parametros: request
 Salida: pagian ver milestone
 Descripcion: para llamar la pagina ver milestone
 """
-
-
 @login_required
 def guardar_convocatoria(request):
     args = {}
     args['usuario'] = request.user
     args['es_admin'] = request.session['es_admin']
+    args.update(csrf(request))
+    mensajeAlerta=None
+    mensajeError=None
     if args['es_admin']:
-        if request.method == 'GET':
-            fecha_max = request.GET['fecMaxima']
-            id_incubacion = request.GET['idIncubacion']
+        if request.method == 'POST':
+            fecha_max = request.POST.get('fecMaxima')
+            id_incubacion = request.POST.get('idIncubacion')
             try:
                 convocatoria = Convocatoria()
                 convocatoria.fecha_creacion = datetime.datetime.now()
                 incubacion = Incubacion.objects.get(id_incubacion=id_incubacion)
+                args['incubacion'] = incubacion
                 convocatoria.fk_incubacion = incubacion
                 convocatoria.fecha_maxima = datetime.datetime.strptime(fecha_max, '%m/%d/%Y')
                 # convocatoria.fecha_maxima = fecha_max
@@ -1506,11 +1508,21 @@ def guardar_convocatoria(request):
                 mensajeError = 'No se creo Convocatoria. La fecha tiene un formato errado. Debe ser (MM/DD/AAAA)'
                 mensajeAlerta = None
 
-        request.session['mensajeError'] = mensajeError
-        request.session['mensajeAlerta'] = mensajeAlerta
-        return HttpResponseRedirect('AdminIncubacion/' + id_incubacion, args)
+            convocatorias_incubacion = Convocatoria.objects.all().filter(fk_incubacion_id=id_incubacion).last()
+            if convocatorias_incubacion is not None:
+                hoy = datetime.datetime.now(timezone.utc)
+                fecha_maxima = convocatorias_incubacion.fecha_maxima
+                if fecha_maxima <= hoy:
+                    args['convocatorias'] = "No hay Convocatoria"
+                else:
+                    args['convocatorias'] = convocatorias_incubacion
+            else:
+                args['convocatorias'] = "No hay Convocatoria"
+
+        args['mensajeError'] = mensajeError
+        args['mensajeAlerta'] = mensajeAlerta
+
+        return render_to_response('admin_ver_incubacion.html', args)
     else:
         return HttpResponseRedirect('InicioIncubaciones')
-
-
 
